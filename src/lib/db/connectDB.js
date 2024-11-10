d24a7a3169f4bdb6ac50bddfaf2e7ce612572978
Shoaib Raza <hafizshoaibraza180@gmail.com>
2024-11-10 10:11:39 +0500

    yr ab set kr diya hai ab error nahi ana chahiye

--- src/lib/db/connectDB.js
+++ src/lib/db/connectDB.js
@@ -2,7 +2,12 @@ import mongoose from "mongoose";
 
 export async function connectDB() {
   try {
-    let connection = await mongoose.connect(process.env.MONGODB_URI);
+    let connection = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      connectTimeoutMS: 20000, // 20 seconds
+    });
+    //  mongoose.connect(process.env.MONGODB_URI);
     console.info("Mongodb Connected");
   } catch (err) {
     console.log("err in connection=>", err);
