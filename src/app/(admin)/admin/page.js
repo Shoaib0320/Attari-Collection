d1e3acaff2cf61a2901b2dd055a2f421f61c9ab1
Shoaib Raza <hafizshoaibraza180@gmail.com>
2024-11-30 22:32:06 +0500

    cors Added

--- src/app/(admin)/admin/page.js
+++ src/app/(admin)/admin/page.js
@@ -44,7 +44,7 @@ export default async function Admin() {
           <br />
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          <RecentOrders />
+          {/* <RecentOrders /> */}
           <TopProducts />
           <div className="lg:col-span-3">
             <SalesSummary />
