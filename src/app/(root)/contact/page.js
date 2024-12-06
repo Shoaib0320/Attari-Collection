1239347e8fe9765274d96cc05cbd57dba0e5f50a
Shoaib Raza <hafizshoaibraza180@gmail.com>
2024-12-06 22:09:01 +0500

    Abhi kaam mein thora masla agaya hai

--- src/app/(root)/contact/page.js
+++ src/app/(root)/contact/page.js
@@ -14,9 +14,9 @@ export default async function Contact() {
         <h1 className="text-4xl font-bold text-center text-gray-800 mb-12">Contact Us</h1>
   
         <div className="max-w-3xl mx-auto bg-white p-8 rounded-lg shadow-lg">
-          <p className="text-lg text-gray-600 text-center mb-8">
-            Have questions? We'd love to hear from you! Please fill out the form below.
-          </p>
+          {/* <p className="text-lg text-gray-600 text-center mb-8">
+            Have questions? We' d love to hear from you! Please fill out the form below.
+          </p> */}
   
           <form className="space-y-6">
             {/* Name Input */}
